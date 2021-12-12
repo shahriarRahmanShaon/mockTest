9e74192de016d0434e1e6db7aaa2d0e4078c9400
shahriarRahmanShaon <shaon463@gmail.com>
2021-12-13 05:55:44 +0600

    openWeather data fetching using okhttp

--- app/src/main/java/com/example/mocktest/MainActivity.kt
+++ app/src/main/java/com/example/mocktest/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mocktest
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -23,12 +25,16 @@ import com.google.android.material.snackbar.Snackbar
 
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
+import okhttp3.*
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    var client = OkHttpClient()
+    lateinit var text2: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +42,10 @@ class MainActivity : AppCompatActivity() {
 
         var button1 = findViewById<Button>(R.id.button)
         var textj = findViewById<TextView>(R.id.textView)
+         text2 = findViewById<TextView>(R.id.textView2)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
 
           requestPermissionLauncher =
             registerForActivityResult(
@@ -61,8 +70,10 @@ class MainActivity : AppCompatActivity() {
                         .addOnSuccessListener { location : Location? ->
                             // Got last known location. In some rare situations this can be null.
                             var lat = location!!.latitude
-                            print(lat)
-                            textj.setText(lat.toString())
+                            var lon = location!!.longitude
+                            var cityName = getAddress(lat, lon)
+                            textj.setText(cityName)
+                            getWeather(cityName)
                         }
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_COARSE_LOCATION) -> {
@@ -79,6 +90,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun getWeather(cityName: String) {
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0e467e5c6beceddf2be0be537b297025"
+        var request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    var getString = response.body!!.string()
+                    text2.setText(getString)
+                    Log.d("json", getString)
+
+                }
+            }
+        })
+
+    }
+
+    private fun getAddress(lat: Double, lng: Double): String {
+        var geocoder = Geocoder(this)
+        var list = geocoder.getFromLocation(lat, lng, 1)
+        return list[0].locality
     }
 
 
