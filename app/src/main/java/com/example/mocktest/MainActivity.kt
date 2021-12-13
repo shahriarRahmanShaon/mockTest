ce67dd607be97df61c6184b13b8a0abc767fc15b
shahriarRahmanShaon <shaon463@gmail.com>
2021-12-13 16:27:19 +0600

    class property and comment modify

--- app/src/main/java/com/example/mocktest/MainActivity.kt
+++ app/src/main/java/com/example/mocktest/MainActivity.kt
@@ -48,16 +48,13 @@ class MainActivity : AppCompatActivity() {
 
         var button1 = findViewById<Button>(R.id.button)
         var textj = findViewById<TextView>(R.id.textView)
-         text2 = findViewById<TextView>(R.id.textView2)
+        text2 = findViewById<TextView>(R.id.textView2)
         imageView = findViewById(R.id.imageView2)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
 
 
-          requestPermissionLauncher =
-            registerForActivityResult(
-                ActivityResultContracts.RequestPermission()
-            ) { isGranted: Boolean ->
+        requestPermissionLauncher = registerForActivityResult( ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
                 if (isGranted) {
                     Log.i("Permission: ", "Granted")
 
@@ -67,31 +64,33 @@ class MainActivity : AppCompatActivity() {
             }
         button1.setOnClickListener{
             when {
-                ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED -> {
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
                     // The permission is granted
-                    // you can go with the flow that requires permission here
-                    fusedLocationClient.lastLocation
-                        .addOnSuccessListener { location : Location? ->
-                            // Got last known location. In some rare situations this can be null.
-                            var lat = location!!.latitude
+                    // doing the task that needs location permission
+
+                    fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+                            // Got last known location with listener in it ignoring error checking
+
+                            var lat = location!!.latitude  // we're sure we gonna get a response, so used !! but not recommended
                             var lon = location!!.longitude
+
+                        // printing to console for checking
                             Log.d("json", lat.toString())
                             Log.d("json", lon.toString())
+
+                        // calling getAdress fucntion with lat and long parameter passed
                             var cityName = getAddress(lat, lon)
+
                             runOnUiThread {
+                                // changing UI in main thread
                                 textj.setText(cityName)
                             }
+                        // calling the openWeather Api with city name in int
                             getWeather(cityName)
                         }
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                    // This case means user previously denied the permission
-                    // So here we can display an explanation to the user
-                    // That why exactly we need this permission
-                    Toast.makeText(this, "message", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "message", Toast.LENGTH_SHORT).show() // showing message why we need location info
                     requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
                 }
                 else -> {
@@ -104,46 +103,61 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getWeather(cityName: String) {
-        var url = "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0e467e5c6beceddf2be0be537b297025"
-        var request = Request.Builder()
+        //function to fetch weather data from openWeatherApi using OKHttps3
+
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0e467e5c6beceddf2be0be537b297025" // making dynamic api url and personal key
+        var request = Request.Builder() // making a builder request with url just made above
             .url(url)
             .build()
-
+        // making request call
         client.newCall(request).enqueue(object : Callback {
+            // Acctually making a request constantly which return two callback named onFailure and Onresponse
+
             override fun onFailure(call: Call, e: IOException) {
+                // We simply catch the error
+
                 e.printStackTrace()
             }
 
             override fun onResponse(call: Call, response: Response) {
+                // Do the things we plan to do if the request is successful
+
                 response.use {
                     if (!response.isSuccessful) throw IOException("Unexpected code $response")
-                    var getString = response.body!!.string()
-                    var gson = GsonBuilder().create()
-                    var result =  gson.fromJson(getString, Welcome::class.java)
-                    Log.d("json", result.weather[0].id.toString())
+
+                    var getString = response.body!!.string() // get the response body from response and map that into string
+
+                    // now we need to map the response into dataclass object
+                    var gson = GsonBuilder().create() // build gson instance
+                    var result =  gson.fromJson(getString, Welcome::class.java) // map the response into welcome.kt data class
+                    Log.d("json", result.weather[0].id.toString()) // printing id of the json response to console for test
                     runOnUiThread {
+                        // changing UI int he main thread
                         text2.setText(result.main.temp.toString())
                     }
 
-                    getBackground(result.weather[0].icon)
+                    getBackground(result.weather[0].icon) // calling getBackgroud function with icon parameter
                     Log.d("json", result.weather[0].icon)
                 }
             }
         })
-
     }
 
     private fun getBackground(iconId: String) {
-        var url = "https://openweathermap.org/img/wn/${iconId}@2x.png"
+        // Function to get background image from openWeatherApi using glide
+
+        var url = "https://openweathermap.org/img/wn/${iconId}@2x.png" // making dynamic url
         runOnUiThread {
+            // As this is an UI element modification it must be done on the main thread
             Glide.with(this).load(url).into(imageView);
         }
     }
 
     private fun getAddress(lat: Double, lng: Double): String {
-        var geocoder = Geocoder(this)
-        var list = geocoder.getFromLocation(lat, lng, 1)
-        return list[0].locality
+        // Function to get location city name using geocoder
+        var geocoder = Geocoder(this) // initializing geocoder for the context
+        var list = geocoder.getFromLocation(lat, lng, 1) // getting location using lat long
+        return list[0].locality  // as it returns a list object we fetch only the local name
     }
 
 
